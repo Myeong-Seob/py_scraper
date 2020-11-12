6757bbbfe7713fa9ebf3db6ec96f288df1c2d8fd
Myeong-Seob <1996ms@naver.com>
2020-11-12 18:03:26 +0900

    #05 saramin - fix location

--- saramin.py
+++ saramin.py
@@ -15,16 +15,32 @@ def get_jobs(word):
     content_box = content_class.find_all("div", {"class": "item_recruit"})
     jobs = []
     for item in content_box:
+        item_value = item["value"]
+        detail_URL = f"http://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx={item_value}&recommend_ids=eJxNj8kBAzEIA6vJH2EuvVPI9t9FSJw1%2BxyPBHhRospxpcUr3%2BuBl%2B2HNLcaf3D74oLg%2BC6zUi8PbFRExUGKISdMovuDwiU5o5IiMmi9fcI9maIHs9PoRbzPqAjOZFshD8ti%2BFiEq88XwVzZYf93M8DTpRZ%2B9u5CA9%2FwBz8VSMo%3D&view_type=search&searchword=python&searchType=search&gz=1&t_ref_content=generic&t_ref=search&paid_fl=n#seq=0"
         h2 = item.find("h2", {"class": "job_tit"})
         title = h2.find("a")["title"]
         job_date = item.find("div", {"class": "job_date"})
         date = job_date.find("span").get_text()
         job_condition = item.find("div", {"class": "job_condition"})
         spans = job_condition.find_all("a")
-        first = spans[0].get_text()
-        second = spans[1].get_text()
-        location = first + " " + second
-        job = {"title": title, "date": date, "location": location}
+        if len(spans) < 2:
+            location = spans[0].get_text()
+        else:
+            first = spans[0].get_text()
+            second = spans[1].get_text()
+            location = first + " " + second
+        strong = item.find("strong", {"class": "corp_name"})
+        company = strong.find("a")["title"]
+        detail = detail_URL
+
+        job = {
+            "FROM": "saramin(사람인)",
+            "title": title,
+            "date": date,
+            "location": location,
+            "company": company,
+            "detail": detail,
+        }
         jobs.append(job)
     return jobs
 
@@ -37,4 +53,4 @@ def init(word):
     print("Done :)")
 
 
-init("React")
+init("인사")
