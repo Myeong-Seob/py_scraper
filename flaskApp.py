b17758e96e251e9ef632cd4ed759911be692e525
Myeong-Seob <1996ms@naver.com>
2020-11-14 02:56:55 +0900

    #13 Flask - css

--- flaskApp.py
+++ flaskApp.py
@@ -3,7 +3,15 @@ from saramin import get_saramin_jobs
 from jobKorea import get_jobKorea_jobs
 
 
+def get_jobs(word):
+    saramin = get_saramin_jobs(word)
+    jobKorea = get_jobKorea_jobs(word)
+    jobs = saramin + jobKorea
+    return jobs
+
+
 app = Flask("Jobs")
+db = {}
 
 
 @app.route("/")
@@ -14,7 +22,16 @@ def index():
 @app.route("/detail")
 def detail():
     word = request.args.get("word")
-    return render_template("detail.html", word=word)
+    if word:
+        existingWord = db.get(word)
+        if existingWord:
+            jobs = existingWord
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template("detail.html", word=word, jobs=jobs, numJobs=len(jobs))
 
 
 app.run(host="0.0.0.0", port=7000, debug=True)
