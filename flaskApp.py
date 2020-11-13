41cfbf9a31004f7e034ac6b38d6bc3340d51e2a5
Myeong-Seob <1996ms@naver.com>
2020-11-14 00:23:29 +0900

    #12 Flask - html

--- flaskApp.py
+++ flaskApp.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, send_file
+from saramin import get_saramin_jobs
+from jobKorea import get_jobKorea_jobs
+
 
 app = Flask("Jobs")
 
@@ -6,6 +9,12 @@ app = Flask("Jobs")
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/detail")
+def detail():
+    word = request.args.get("word")
+    return render_template("detail.html", word=word)
 
 
 app.run(host="0.0.0.0", port=7000, debug=True)
